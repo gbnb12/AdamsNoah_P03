aae98a064abe83054a5a73374eb93f4f8e5b1597
N <adamsnoah999@gmail.com>
2022-04-28 21:18:14 -0500

    Created Player Movement, Jumping, and Directions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        // flips palyer left or right
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // player jumps
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
 }
